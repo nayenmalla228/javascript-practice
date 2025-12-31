4ee8a01a7200b21c33dfcea40e5d209c7021f6fa
Pratyaksha Kumar Malla <nayenkumar228@gmail.com>
2025-12-31 13:07:23 +0545

    learned about the template literals

--- variable.js
+++ variable.js
@@ -1,2 +1,9 @@
 let a = 10;
 console.log(a);
+const b = 20;
+console.log("the value of b is :", b);
+console.log(`the value of b is ${b}`);
+const firstName = "nayen";
+const lastName = "malla";
+console.log("The first name is " + firstName + " and last name is " + lastName);
+console.log(`The firstname is ${firstName} and lastname is ${lastName}`);
