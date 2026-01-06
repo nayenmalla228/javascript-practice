10e1310c57e5092d3119d8601369cf9561d52f6e
Pratyaksha Kumar Malla <nayenkumar228@gmail.com>
2026-01-06 14:42:48 +0545

    learn functions

---
+++ function.js
@@ -0,0 +1,23 @@
+//addition using function
+function add(a, b) {
+  return a + b;
+}
+const sum = add(10, 19);
+console.log(sum);
+
+//substraction using function
+
+function sub(c, d) {
+  return c - d;
+}
+const subs = sub(10, 2);
+console.log(subs);
+
+const multiply = (a, b) => {  //arrow function
+  return a * b;
+};
+const product = multiply(5, 2);
+console.log(product);
+
+const division =(a,b)=>a/b;
+console.log(division(50,25));
